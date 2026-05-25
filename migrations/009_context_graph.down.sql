37c20ad5eaca9f78631ce4c99c2be4f602237142
homan <4878874+hman-pro@users.noreply.github.com>
2026-05-25 16:13:32 +0300

    feat(schema): migration 009 adds context graph tables

---
+++ migrations/009_context_graph.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS context_participants;
+DROP TABLE IF EXISTS context_chunks;
+DROP TABLE IF EXISTS context_item_versions;
+DROP TABLE IF EXISTS context_items;
+DROP TABLE IF EXISTS person_identities;
+DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS context_source_state;
+DROP TABLE IF EXISTS context_sources;
